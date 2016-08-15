478b899afefee5a1ee36f5e08cd77894cabbc30d
Talal Talhouk <talal@sbcglobal.net>
2016-08-15 14:09:51 -0700

    update the ClientApp file

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,20 +1,15 @@
-// /* globals React ReactDOM*/
-// for standard
 var React = require('react')
 var ReactDOM = require('react-dom')
 var MyTitle = require('./MyTitle')
 
-// stateless component
 var MyFirstComponent = function () {
   return (
     <div>
-      <MyTitle title="Whatevs" color="rebeccapurple" />
-      <MyTitle title="LOL" color="papayawhip" />
-      <MyTitle title="OMGLOLWTFBBQ" color="#f06d06" />
+      <MyTitle title='Whatevs' color='rebeccapurple' />
+      <MyTitle title='LOL' color='papayawhip' />
+      <MyTitle title='OMGLOLWTFBBQ' color='#f06d06' />
     </div>
   )
 }
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
-
-// note semicolons are mising because using standard to add them
