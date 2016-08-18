b81fe6c75e81d16a8d158150e982055948d26671
Talal Talhouk <talal@sbcglobal.net>
2016-08-18 12:39:52 -0700

    add Store and applied it to header search and app

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,6 +1,7 @@
 const React = require('react')
 const ShowCard = require('./ShowCard')
 const Header = require('./Header')
+const { connector } = require('./Store')
 // being passed as props.route from ClientApp
 // 'const data = require('../public/data')'
 
@@ -22,31 +23,44 @@ const Header = require('./Header')
 // ES6 Syntax for State Components
 // class Search extends React.Component {
 
-const { object } = React.PropTypes
+const { object, string } = React.PropTypes
 
 const Search = React.createClass({
-  getInitialState () {
-    return {
-      searchTerm: ''
-    }
-  },
-  handleSearchTermChange (searchTerm) {
-    this.setState({searchTerm: searchTerm})
-  },
+
+  // handle with store...
+  // getInitialState () {
+  //   return {
+  //     searchTerm: ''
+  //   }
+  // },
+
+  // no longer handling state of this
+  // handleSearchTermChange (searchTerm) {
+  //   this.setState({searchTerm: searchTerm})
+  // },
+  // for Header
+  // Original Code
+  //        <Header
+  //           handleSearchTermChange={this.handleSearchTermChange}
+  //           searchTerm={this.state.searchTerm}
+  //           showSearch />
+  // {this.props.route.shows
+  // .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+  // .map((show) => (
+  //   <ShowCard {...show} key={show.imdbID} />
+  // ))}
+
   propTypes: {
-    route: object
+    route: object,
+    searchTerm: string
   },
   render () {
     return (
       <div className='container'>
-        <Header
-          handleSearchTermChange={this.handleSearchTermChange}
-          searchTerm={this.state.searchTerm}
-          showSearch />
+        <Header showSearch />
         <div className='shows'>
-          {console.log(this.props.route.shows)}
           {this.props.route.shows
-            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0)
             .map((show) => (
               <ShowCard {...show} key={show.imdbID} />
           ))}
@@ -56,4 +70,4 @@ const Search = React.createClass({
   }
 })
 
-module.exports = Search
+module.exports = connector(Search)
